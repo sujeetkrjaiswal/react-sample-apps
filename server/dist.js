4ea222dddb90c4bd927d065e8249f935b44fb393
Sujeet Kumar Jaiswal <sujeet_jaiswal@outlook.com>
2017-05-09 02:30:00 +0530

    Initial commit with dev and prod setup. It has a very basic example of react. Real world example is due for addition

---
+++ server/dist.js
@@ -0,0 +1,24 @@
+import browserSync from 'browser-sync';
+import historyApiFallback from 'connect-history-api-fallback';
+import {chalkProcessing} from '../tools/chalkConfig';
+
+/* eslint-disable no-console */
+
+console.log(chalkProcessing('Opening production build...'));
+
+// Run Browsersync
+browserSync({
+  port: 4000,
+  ui: {
+    port: 4001
+  },
+  server: {
+    baseDir: 'dist'
+  },
+
+  files: [
+    'src/*.html'
+  ],
+
+  middleware: [historyApiFallback()]
+});
